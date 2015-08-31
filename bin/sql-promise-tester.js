2f369c20fbc23c76e439fd4c1e044ebbbefa0cc3
Emilio <emilioplatzer@gmail.com>
2015-08-31 18:13:20 -0300

    primeros tests.

--- bin/sql-promise-tester.js
+++ bin/sql-promise-tester.js
@@ -7,10 +7,34 @@ var sqlPromiseTester = {};
 
 var expect = require('expect.js');
 
-sqlPromiseTester.test = function(motor){
+sqlPromiseTester.test = function(motor, opts){
     describe('sql-promise-tester '+motor.name, function(){
-        it('connect', function(){
-            expect("no implementado").to.eql("aun");
+        it('must provide defaultPort', function(){
+            expect('defaultPort' in motor).to.be.ok();
+        });
+        it('connect', function(done){
+            motor.connect(opts.connOpts||{
+                user:'test_user',
+                password:'test_pass',
+                database:'test_db',
+                host:'localhost',
+                port:motor.defaultPort
+            }).then(function(){
+                done();
+            }).catch(done);
+        });
+        it('not connect with bad connection parameters', function(done){
+            motor.connect(opts.badConnOpts||{
+                user:'test_user',
+                password:'BAD PASS',
+                database:'test_db',
+                host:'localhost',
+                port:motor.defaultPort
+            }).then(function(){
+                done(new Error('must not connect'));
+            }).catch(function(err){
+                done();
+            });
         });
     });
 };
