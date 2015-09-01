40b6828c5432c4902372ee52bd01446b37e27951
Emilio <emilioplatzer@gmail.com>
2015-09-01 11:34:02 -0300

    Agrego inicializador al tester.

--- bin/sql-promise-tester.js
+++ bin/sql-promise-tester.js
@@ -8,18 +8,26 @@ var sqlPromiseTester = {};
 var expect = require('expect.js');
 
 sqlPromiseTester.test = function(motor, opts){
+    var defaultConnOpts={
+        user:'test_user',
+        password:'test_pass',
+        database:'test_db',
+        host:'localhost',
+        port:motor.defaultPort
+    };
     describe('sql-promise-tester '+motor.name, function(){
+        before(function(done){
+            if(opts.prepare){
+                opts.prepare().then(done,done);
+            }else{
+                done();
+            }
+        });
         it('must provide defaultPort', function(){
             expect('defaultPort' in motor).to.be.ok();
         });
         it('connect', function(done){
-            motor.connect(opts.connOpts||{
-                user:'test_user',
-                password:'test_pass',
-                database:'test_db',
-                host:'localhost',
-                port:motor.defaultPort
-            }).then(function(){
+            motor.connect(opts.connOpts||defaultConnOpts).then(function(){
                 done();
             }).catch(done);
         });
@@ -35,6 +43,23 @@ sqlPromiseTester.test = function(motor, opts){
             }).catch(function(err){
                 done();
             });
+        });
+        describe('connectected tests', function(done){
+            var conn;
+            before(function(done){
+                motor.connect(opts.badConnOpts||defaultConnOpts).then(function(obtainedConn){
+                    conn=obtainedConn;
+                    done();
+                }).catch(done);
+            });
+            it('must create table',function(done){
+                var cursor=conn.query("CREATE TABLE example1(id integer primary key, datum text)");
+                expect(cursor.execute).to.be.a(Function);
+                cursor.execute().then(function(result){
+                    console.log('result',result);
+                    done();
+                }).catch(done);
+            })
         });
     });
 };
