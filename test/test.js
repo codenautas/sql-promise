a2a08faff7404eeea3395cb3b878f3e366e05241
Diego F.(i7) <diegoefe@gmail.com>
2015-09-14 14:07:15 -0300

    Agrego estructura para test

---
+++ test/test.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var sqlPromise=require('..');
+var expect = require('expect.js');
+
+// var motor = new Motor;
+// motor.connect().then(function(con) {
+   // console.log("connected"); 
+// }).catch(function(e) {
+   // console.log("Err", e);
+// });
+
+
+describe('sql-promise interface tests', function(){
+    describe('tests of non-implemented ABC\'s', function(){
+        it('should throw exceptions on unimplemented methods', function(done){
+                done();
+            });
+        });
+    });
+
