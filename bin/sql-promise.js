8ba76f5bd01d10cead05d3978777bf7a9a376bd9
Emilio <emilioplatzer@gmail.com>
2015-09-14 23:22:58 -0300

    Posible forma de encarar el motor

--- bin/sql-promise.js
+++ bin/sql-promise.js
@@ -7,6 +7,52 @@ var sqlPromise = {};
 
 var Promises = require('best-promise');
 
+sqlPromise.register = function register(motorName, motor){
+    this.motors = this.motors || {};
+    this.motors[motorName] = motor;
+}
+
+sqlPromise.connect = function connect(connOpts){
+    var sqlLib = this;
+    var motor = this.motors[connOpts.motor];
+    return Promises.start(function(){
+        return motor.connect(connOpts);
+    }).then(function(internal){
+        return new sqlLib.Connection(sqlLib, internal, motor);
+    });
+}
+
+sqlPromise.Connection = function Connection(sqlLib, internal, motor) {
+    this.prepare = function prepare(sqlSentence){
+        return Promises.start(function(){
+            return motor.prepare(internal, sqlSentence);
+        }).then(function(internalPreparedQuery){
+            return new sqlLib.PreparedQuery(sqlLib, internalPreparedQuery, motor);
+        });
+    }
+};
+
+sqlPromise.PreparedQuery = function PreparedQuery(sqlLib, internal, motor) {
+    this.query = function query(data){
+        return Promises.start(function(){
+            return motor.query(internal, data);
+        }).then(function(internalQuery){
+            return new sqlLib.Query(sqlLib, internalQuery, motor);
+        });
+    }
+};
+
+sqlPromise.Query = function Query(sqlLib, internal, motor) {
+    this.fetchAll = function fetchAll(){
+        return Promises.start(function(){
+            return motor.fetchAll(internal);
+        }).then(function(result){
+            return result;
+        });
+    }
+};
+
+
 function OpaqueObject() {
     this.internalState = {};
 };
@@ -41,151 +87,5 @@ sqlPromise.Motor = function Motor() {
         return Promises.reject("Motor.fetchRowByRow() should return a DbResult");
     };
 };
-
-
-//console.log(new DbQuery().internalState);
-
-// sqlPromise.motorName = 'generic';
-
-// sqlPromise.Connection = function Connection(connOpts, connection, done, motor){
-    // this.motor = motor;
-    // this.fromPool = connOpts==='pool';
-    // var self = this;
-    // var assignFunctionsPostConnect = function assignFunctionsPostConnect(){
-        // existing functions
-        // self.done = function(){
-            // if(motor.debug.pool){
-                // motor.debug.pool[connection.secretKey].count--;
-            // }
-            // return done.apply(connection,arguments);
-        // };
-        // self.query = function query(){
-            // if(motor.log){
-                // var sql=arguments[0];
-                // motor.log('------');
-                // if(arguments[1]){
-                    // motor.log('-- '+sql);
-                    // motor.log('-- '+JSON.stringify(arguments[1]));
-                    // for(var i=1; i<=arguments[1].length; i++){
-                        // var valor=arguments[1][i-1];
-                        // if(typeof valor === 'string'){
-                            // valor="'"+valor.replace(/'/g,"''")+"'";
-                        // }
-                        // sql=sql.replace(new RegExp('\\$'+i+'\\b'), valor);
-                    // }
-                // }
-                // motor.log(sql+';');
-            // }
-            // var internalCursor = motor.getQuery(connection, arguments);
-            // return new motor.Query(internalCursor, self);
-        // };
-    // };
-    // motor.allowAccessInternalIfDebugging(self, {connection:connection, pool:this.fromPool, done:done});
-    // if(motor.debug.pool && this.fromPool){
-        // if(motor.debug.pool===true){
-            // motor.debug.pool={};
-        // }
-        // if(!(connection.secretKey in motor.debug.pool)){
-            // motor.debug.pool[connection.secretKey] = {connection:connection, count:0};
-        // }
-        // motor.debug.pool[connection.secretKey].count++;
-    // }
-    // assignFunctionsPostConnect();
-// };
-
-// sqlPromise.Query = function Query(query, connection){
-    // var self = this;
-    // connection.motor.allowAccessInternalIfDebugging(self, {query: query, connection:connection});
-    // this.execute = function execute(callbackForEachRow, adapterName){
-        // connection.motor.log('Query.execute');
-        // if(callbackForEachRow && !(callbackForEachRow instanceof Function)){
-            // if(adapterName){
-                // return Promises.reject(new Error("Query.execute() must recive optional callback function and optional adapterName"));
-            // }
-            // adapterName=callbackForEachRow;
-            // callbackForEachRow=null;
-        // }
-        // var adapter = connection.motor.queryAdapters[adapterName||'normal'];
-        // return connection.motor.makePromiseFetcher(query, callbackForEachRow, function(resolve,reject){
-            // return function(result){
-                // result.connection = connection;
-                // if(connection.motor.log){
-                    // connection.motor.log('-- '+JSON.stringify(result.rows));
-                // }
-                // adapter(result, resolve, reject);
-            // }
-        // });
-    // };
-    //new functions
-    // this.fetchOneRowIfExists = this.execute.bind(this,'upto1');
-    // this.fetchUniqueRow      = this.execute.bind(this,'row');
-    // this.fetchUniqueValue    = this.execute.bind(this,'value');
-    // this.fetchAll            = this.execute.bind(this,'normal');
-    // this.fetchRowByRow       = function fetchRowByRow(callback){
-        //connection.motor.log('Query.onRow');
-        // if(!(callback instanceof Function)){
-            // var err=new Error('fetchRowByRow must recive a callback that executes for each row');
-            // err.code='39004!';
-            // return Promises.reject(err);
-        // }
-        // return this.execute(callback);
-    // };
-    // this.onRow = this.fetchRowByRow;
-    // /* why this then function is needed?
-     // *   pg.Client.query is synchronic (not need to recive a callback function) then not need to return a Promise
-     // *   but pg-promise-strict always returns a "theneable". Then "then" is here. 
-     // */
-    // if(connection.motor.easy){
-        // this.then = function then(callback,callbackE){
-            // delete this.then;
-            // delete this.catch;
-            // return this.execute().then(callback,callbackE);
-        // };
-    // }
-// };
-
-// sqlPromise.buildQueryCounterAdapter = function buildQueryCounterAdapter(minCountRow, maxCountRow, expectText, callbackOtherControl){
-    // return function queryCounterAdapter(result, resolve, reject){ 
-        // if(result.rows.length<minCountRow || result.rows.length>maxCountRow ){
-            // var err=new Error('query expects '+expectText+' and obtains '+result.rows.length+' rows');
-            // err.code='54011!';
-            // reject(err);
-        // }else{
-            // if(callbackOtherControl){
-                // callbackOtherControl(result, resolve, reject);
-            // }else{
-                // result.row = result.rows[0];
-                // delete result.rows;
-                // resolve(result);
-            // }
-        // }
-    // };
-// }
-
-// sqlPromise.queryAdapters = {
-    // normal: function normalQueryAdapter(result, resolve/*, reject*/){ 
-        // resolve(result);
-    // },
-    // upto1:sqlPromise.buildQueryCounterAdapter(0,1,'up to one row'),
-    // row:sqlPromise.buildQueryCounterAdapter(1,1,'one row'),
-    // value:sqlPromise.buildQueryCounterAdapter(1,1,'one row (with one field)',function(result, resolve, reject){
-        // if(result.fields.length!==1){
-            // var err=new Error('query expects one field and obtains '+result.fields.length);
-            // err.code='54U11!';
-            // reject(err);
-        // }else{
-            // var row = result.rows[0];
-            // result.value = row[result.fields[0].name];
-            // delete result.rows;
-            // resolve(result);
-        // }
-    // })
-// };
-
-// sqlPromise.allowAccessInternalIfDebugging = function allowAccessInternalIfDebugging(self, internals){
-    // if(this.debug[self.constructor.name]){
-        // self.internals = internals;
-    // }
-// };
 
 module.exports = sqlPromise;
--- test/test.js
+++ test/test.js
@@ -3,6 +3,58 @@
 var sqlPromise=require('..');
 var expect = require('expect.js');
 var Promises = require('best-promise');
+var sinon = require('sinon');
+
+var mockConnectionInternal = {internal:'connnnnnn'};
+var mockPreparedInternal = {'internal':'prepppppprprprprprpr'};
+var mockQueryInternal = {internal:'queryyyyyyyyyyyyiyiyiyiyiy', internalbis:'2.2.2.2.2'};
+
+var mockMotor={
+    connect: sinon.stub().returns(Promises.resolve(mockConnectionInternal)),
+    prepare: sinon.stub().returns(Promises.resolve(mockPreparedInternal)),
+    query: sinon.stub().returns(Promises.resolve(mockQueryInternal)),
+    fetchAll: sinon.stub().returns(Promises.resolve({rowCount:1, rows:[{datum:3}]})),
+    mockConnectOptions: {thisOptions:'this_opts'}
+};
+
+sqlPromise.register('mock', mockMotor);
+
+describe('sql-promise motor test', function(){
+    it('should create a connection', function(done){
+        var connOpts = {motor:'mock', param1:1, param2:'two'};
+        var prom = sqlPromise.connect(connOpts);
+        expect(prom).to.be.a(Promises.Promise);
+        prom.then(function(conn){
+            expect(conn).to.be.a(sqlPromise.Connection)
+            expect(mockMotor.connect.callCount).to.be(1);
+            expect(mockMotor.connect.firstCall.args).to.eql([connOpts]);
+            var prom = conn.prepare("SELECT 1 + $1 as datum");
+            expect(prom).to.be.a(Promises.Promise);
+            return prom;
+        }).then(function(preparedQuery){
+            expect(preparedQuery).to.be.a(sqlPromise.PreparedQuery);
+            expect(mockMotor.prepare.callCount).to.be(1);
+            expect(mockMotor.prepare.firstCall.args).to.eql([mockConnectionInternal, "SELECT 1 + $1 as datum"]);
+            var prom = preparedQuery.query([2]);
+            expect(prom).to.be.a(Promises.Promise);
+            return prom;
+        }).then(function(query){
+            expect(query).to.be.a(sqlPromise.Query);
+            expect(mockMotor.query.callCount).to.be(1);
+            expect(mockMotor.query.firstCall.args).to.eql([mockPreparedInternal, [2]]);
+            var prom = query.fetchAll();
+            expect(prom).to.be.a(Promises.Promise);
+            return prom;
+        }).then(function(result){
+            expect(mockMotor.fetchAll.callCount).to.be(1);
+            expect(mockMotor.fetchAll.firstCall.args).to.eql([mockQueryInternal]);
+            expect(result.rows).to.eql([{datum:3}]);
+            expect(result.rowCount).to.eql(1);
+        }).then(function(){
+            done();
+        }).catch(done);
+    });
+});
 
 describe('sql-promise interface tests', function(){
     it('should throw exceptions on unimplemented methods', function(done){
